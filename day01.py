8e35229ac991c23558308986e0a239660504721e
Jay Sanghavi <jay.sanghavil@outlook.com>
2025-10-31 11:52:35 -0700

    added task 5 to day01.py

--- day01.py
+++ day01.py
@@ -1,7 +1,9 @@
 # Task 1:
 print("Hello World\n")
 # Task 2:
-print("Hello " + input("What is your name?") + "!\n")
+print("Hello " + input("What is your name? ") + "!\n")
 # Task 3:
 name = "Jack\n"
 print(name)
+# Task 5:
+print(len(input("What is your name? ")))
